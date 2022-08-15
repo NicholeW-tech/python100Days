db7fd5b2bc1041c38776f3b29301e2785ec91e35
NicholeW-tech <85132041+NicholeW-tech@users.noreply.github.com>
2022-08-15 14:49:23 -0400

    Create main.py

---
+++ day032/main.py
@@ -0,0 +1,31 @@
+import smtplib
+import datetime as dt
+import random
+import pandas
+
+
+
+now = dt.datetime.now()
+date = (now.month, now.day)
+
+
+# HINT 2: Use pandas to read the birthdays.csv
+data = pandas.read_csv("birthdays.csv")
+
+birthday_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if date in birthday_dict:
+    birthday_person = birthday_dict[date]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy Birthday!\n\n{contents}")
+
