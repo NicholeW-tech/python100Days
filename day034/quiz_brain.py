fd24392de2df4f50db7e18c10c51a695e13a34d0
NicholeW-tech <85132041+NicholeW-tech@users.noreply.github.com>
2022-08-15 15:30:37 -0400

    Update quiz_brain.py

--- day034/quiz_brain.py
+++ day034/quiz_brain.py
@@ -1,27 +1,28 @@
+import html
 class QuizBrain:
+
     def __init__(self, q_list):
         self.question_number = 0
-        # self.questions_list = input(f"Q.{self.question_number + 1}: {question_bank[0].text} (True/False)?: ").lower()
-        self.questions_list = q_list
         self.score = 0
+        self.question_list = q_list
+        self.current_question = None
 
     def still_has_questions(self):
-        return self.question_number < len(self.questions_list)
-
+        return self.question_number < len(self.question_list)
 
     def next_question(self):
-        current_question = self.questions_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_ans = input(f"Q.{self.question_number }: {current_question.text} (True/False)?: ").lower()
-        self.check_answer(user_ans, current_question.answer)
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text} (True/False): "
+        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
+        # self.check_answer(user_answer)
 
-    def check_answer(self, user_answer, correct_answer):
-        if user_answer == correct_answer.lower():
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
+        if user_answer== correct_answer:
             self.score += 1
-            print("That's right!")
+            return True
         else:
-            print("That's incorrect")
-        print(f"The correct answer was: {correct_answer}")
-        print(f"Your current score is : {self.score}/{self.question_number}")
-        print("\n")
+            return False
 
