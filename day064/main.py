26c061d916b33866a432d6855ceb0b10d6cbe9f7
NicholeW-tech <85132041+NicholeW-tech@users.noreply.github.com>
2022-08-15 22:43:32 -0400

    Create main.py

---
+++ day064/main.py
@@ -0,0 +1,130 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET KEY
+Bootstrap(app)
+
+##CREATE DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##CREATE TABLE
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=False)
+db.create_all()
+
+
+## After adding the new_movie the code needs to be commented out/deleted.
+## So you are not trying to add the same movie twice.
+# new_movie = Movie(
+#     id=2,
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating='7.3',
+#     ranking='10',
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+# db.session.add(new_movie)
+# db.session.commit()
+
+class MovieForm(FlaskForm):
+    title = StringField('Movie name', validators=[DataRequired()])
+    year = StringField('year', validators=[DataRequired()])
+    description = StringField('Movie description', validators=[DataRequired()])
+    rating = StringField('Movie rating', validators=[DataRequired()])
+    ranking = StringField('Movie ranking', validators=[DataRequired()])
+    review = StringField('Movie review', validators=[DataRequired()])
+    img_url = StringField('Movie image URL', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route("/")
+def home():
+    all_movies = Movie.query.all()
+    return render_template("index.html", movies=all_movies)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("home"))
+
+
+class FindMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = FindMovieForm()
+
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+
+    return render_template("add.html", form=form)
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    form = MovieForm()
+    if request.method == "POST":
+        # UPDATE RECORD
+        movie_id = request.form["id"]
+        movie_to_update = Movie.query.get(movie_id)
+        movie_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    movie_id = request.args.get('id')
+    movie_selected = Movie.query.get(movie_id)
+    return render_template("edit.html", movie=movie_selected, form=form)
+
+@app.route('/delete')
+def delete():
+    movie_id = request.args.get('id')
+    movie_to_delete = Movie.query.get(movie_id)
+    db.session.delete(movie_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
